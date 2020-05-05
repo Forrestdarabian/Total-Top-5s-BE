5e4d43897f7c158e20a4d51f0d46e5ad05918068
Forrest-Darabian <mr.smiley72@yahoo.com>
2020-05-05 14:18:29 -0700

    Project Set Up

---
+++ server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const server = express();
+// const usersRouter = require("./users/usersRouter");
+// const celebsRouter = require("./celebs/celebsRouter");
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+// server.use("/api/users", usersRouter);
+// server.use("/api/celebs", celebsRouter);
+
+server.get("/", (req, res) => {
+  res.send("Server is up and running :)");
+});
+
+module.exports = server;
