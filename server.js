7f557ee81dc18f3edfb1de758d3a38bb58370ca7
Forrest-Darabian <mr.smiley72@yahoo.com>
2020-05-06 02:33:45 -0700

    Ready for Deployment

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const server = express();
-// const usersRouter = require("./users/usersRouter");
+const usersRouter = require("./users/usersRouter");
 // const celebsRouter = require("./celebs/celebsRouter");
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-// server.use("/api/users", usersRouter);
+server.use("/api/users", usersRouter);
 // server.use("/api/celebs", celebsRouter);
 
 server.get("/", (req, res) => {
